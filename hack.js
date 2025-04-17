b9c66d8cf7476d992f5e1522031f77537dab78d2
emmet_748 <146335654+emmet748@users.noreply.github.com>
2025-04-17 18:43:37 +0200

    Add files via upload

---
+++ hack.js
@@ -0,0 +1,4 @@
+document.body.style.backgroundColor = "black";
+document.body.innerHTML = "<h1 style='czarny</h1>";
+alert("Wstrzyknięty JS z GitHub Pages!");
+console.log("Cookies:", document.cookie);
