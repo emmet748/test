90e0610098689d790143bbf70d15b9de6c4f18bc
emmet_748 <146335654+emmet748@users.noreply.github.com>
2025-04-17 18:46:30 +0200

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,4 +1,3 @@
-document.body.style.backgroundColor = "black";
-document.body.innerHTML = "<h1 style='czarny</h1>";
+
 alert("Wstrzyknięty JS z GitHub Pages!");
 console.log("Cookies:", document.cookie);
